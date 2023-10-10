22295789a7cd6f76fbff086c1a10f827884785fd
Thi <thi@ideta.io>
2023-10-10 02:06:13 +0200

    fix: overflow inside callout

--- src/components/BlockCallout.tsx
+++ src/components/BlockCallout.tsx
@@ -16,7 +16,7 @@ export default function BlockCallout(props: {
         className={cn('flex rounded-md', mapColorClass(props.color) || 'border border-gray-200')}
       >
         {props.icon && <div className="text-2xl pl-4 py-3">{props.icon}</div>}
-        <div className="pl-2 pr-4 w-full">
+        <div className="pl-2 pr-4 w-0 flex-1">
           <div className="mt-4 mb-3">{props.text}</div>
           {!!props.children && <div className="m2it-inside-box">{props.children}</div>}
           <div className="my-4"></div>

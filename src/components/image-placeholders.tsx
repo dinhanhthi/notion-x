51f65096ca6e417156a18e32362697ec88f1cc25
Thi <thi@ideta.io>
2023-11-08 00:43:42 +0100

    chore

--- src/components/image-placeholders.tsx
+++ src/components/image-placeholders.tsx
@@ -7,11 +7,11 @@ export function ImagePlaceholderPostHeader() {
   return (
     <div
       className={cn(
-        'bg-gray-100 flex items-center justify-center animate-pulse w-full h-full',
+        'bg-gradient-to-r from-sky-500 to-indigo-500 flex items-center justify-center w-full h-full',
         'flex flex-col'
       )}
     >
-      <AiOutlineLoading3Quarters className="text-[70px] text-slate-400 animate-spin" />
+      <AiOutlineLoading3Quarters className="text-[60px] text-white animate-spin" />
     </div>
   )
 }

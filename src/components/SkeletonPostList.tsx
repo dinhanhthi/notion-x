65ddf23af18bb4d9e1738b5c2633f6c34a84b703
Thi <dinhanhthi@gmail.com>
2024-03-17 10:48:48 +0100

    style: SkeletonPostList

--- src/components/SkeletonPostList.tsx
+++ src/components/SkeletonPostList.tsx
@@ -16,6 +16,7 @@ type SkeletonPostListProps = {
   listStyle?: PostListStyle
   options?: {
     className?: string
+    postContainerClassName?: string
   }
 }
 
@@ -24,7 +25,9 @@ export default function SkeletonPostList(props: SkeletonPostListProps) {
     <>
       {(!props.listStyle || props.listStyle === 'default') && (
         <div className={cn(props.options?.className || postListGridCLass, 'animate-pulse')}>
-          {Array.from({ length: props.count }).map((_, i) => getSkeleton(i, props.postType))}
+          {Array.from({ length: props.count }).map((_, i) =>
+            getSkeleton(i, props.postType, props.options?.postContainerClassName)
+          )}
         </div>
       )}
       {props.listStyle === 'carousel' && (
@@ -32,7 +35,7 @@ export default function SkeletonPostList(props: SkeletonPostListProps) {
           items={Array.from({ length: props.count }).map((_, i) => ({ id: i }))}
           renderItem={({ item, isSnapPoint }) => (
             <CarouselItem key={item.id} isSnapPoint={isSnapPoint} widthClass={'w-80'}>
-              {getSkeleton(item.id, props.postType)}
+              {getSkeleton(item.id, props.postType, props.options?.postContainerClassName)}
             </CarouselItem>
           )}
         />
@@ -41,7 +44,7 @@ export default function SkeletonPostList(props: SkeletonPostListProps) {
   )
 }
 
-function getSkeleton(key: number | string, postType?: PostType) {
+function getSkeleton(key: number | string, postType?: PostType, postContainerClassName?: string) {
   switch (postType) {
     case 'PostTitleCateDate':
       return <PostTitleCateDateSkeleton key={key} />
@@ -53,7 +56,7 @@ function getSkeleton(key: number | string, postType?: PostType) {
       return <PostImageBackgroundSkeleton key={key} />
 
     case 'PostCardWave':
-      return <PostCardWaveSkeleton key={key} />
+      return <PostCardWaveSkeleton key={key} postContainerClassName={postContainerClassName} />
 
     case 'PostSimple':
       return <PostSimpleSkeleton key={key} />
@@ -63,62 +66,65 @@ function getSkeleton(key: number | string, postType?: PostType) {
   }
 }
 
-const PostCardWaveSkeleton = () => (
-  <div className="flex flex-col justify-center">
-    <div className={cn('flex items-center justify-center w-full rounded-md bg-slate-200', 'h-32')}>
-      <div className="w-full flex flex-col items-center gap-1 p-3">
-        <div className="h-4 w-full rounded-md bg-slate-300"></div>
-        <div className="h-4 w-3/4 rounded-md bg-slate-300"></div>
-      </div>
+const PostCardWaveSkeleton = (props: { postContainerClassName?: string }) => (
+  <div
+    className={cn(
+      'flex items-center justify-center w-full rounded-[12px] h-32 shadow-sm',
+      props.postContainerClassName
+    )}
+  >
+    <div className="w-full flex flex-col items-center gap-2 p-3">
+      <div className="h-4 w-full rounded-xl bg-slate-200"></div>
+      <div className="h-4 w-3/4 rounded-xl bg-slate-200"></div>
     </div>
   </div>
 )
 
-const PostTitleCateDateSkeleton = () => (
-  <div className="flex flex-col justify-center">
-    <div className={cn('w-full rounded-md bg-slate-200', TCDFIHeightClass)}></div>
+const PostTitleCateDateSkeleton = (props: { postContainerClassName?: string }) => (
+  <div className={cn('flex flex-col justify-center', props.postContainerClassName)}>
+    <div className={cn('w-full rounded-xl bg-slate-200', TCDFIHeightClass)}></div>
     <div className="flex flex-col items-center gap-1 p-2">
-      <div className="h-4 w-full rounded-md bg-slate-200"></div>
-      <div className="h-4 w-3/4 rounded-md bg-slate-200"></div>
+      <div className="h-4 w-full rounded-xl bg-slate-200"></div>
+      <div className="h-4 w-3/4 rounded-xl bg-slate-200"></div>
     </div>
   </div>
 )
 
-const PostCardWhiteBgBigSkeleton = () => (
-  <div className="flex flex-col justify-center">
-    <div className={cn('w-full rounded-md bg-slate-200', CWBBHeightClass)}></div>
+const PostCardWhiteBgBigSkeleton = (props: { postContainerClassName?: string }) => (
+  <div className={cn('flex flex-col justify-center', props.postContainerClassName)}>
+    <div className={cn('w-full rounded-xl bg-slate-200', CWBBHeightClass)}></div>
     <div className="flex flex-col items-center gap-1 p-2">
-      <div className="h-4 w-full rounded-md bg-slate-200"></div>
+      <div className="h-4 w-full rounded-xl bg-slate-200"></div>
 
       {/* excerpt */}
       <div className="w-full mt-2 px-2 flex flex-col items-center gap-1">
-        <div className="h-2 w-full rounded-md bg-slate-200"></div>
-        <div className="h-2 w-full rounded-md bg-slate-200"></div>
-        <div className="h-2 w-3/4 rounded-md bg-slate-200"></div>
+        <div className="h-2 w-full rounded-xl bg-slate-200"></div>
+        <div className="h-2 w-full rounded-xl bg-slate-200"></div>
+        <div className="h-2 w-3/4 rounded-xl bg-slate-200"></div>
       </div>
     </div>
   </div>
 )
 
-const PostImageBackgroundSkeleton = () => (
-  <div className="flex flex-col justify-center">
-    <div className={cn('relative w-full rounded-md bg-slate-200', PIBHeightClass)}>
+const PostImageBackgroundSkeleton = (props: { postContainerClassName?: string }) => (
+  <div className={cn('flex flex-col justify-center', props.postContainerClassName)}>
+    <div className={cn('relative w-full rounded-xl bg-slate-200', PIBHeightClass)}>
       <div className="absolute bottom-0 left-0 w-full flex flex-col items-center gap-1 p-3">
-        <div className="h-4 w-full rounded-md bg-slate-300"></div>
-        <div className="h-4 w-3/4 rounded-md bg-slate-300"></div>
+        <div className="h-4 w-full rounded-xl bg-slate-300"></div>
+        <div className="h-4 w-3/4 rounded-xl bg-slate-300"></div>
       </div>
     </div>
   </div>
 )
 
-const PostSimpleSkeleton = () => (
-  <div className="flex items-center gap-3 py-3 px-2">
+const PostSimpleSkeleton = (props: { postContainerClassName?: string }) => (
+  <div className={cn('flex items-center gap-3 py-3 px-2', props.postContainerClassName)}>
     <div>
       <HiOutlineDocumentText className="text-xl text-slate-700" />
     </div>
     <div className="flex-1 flex justify-start">
-      <div className="h-6 w-3/4 rounded-md bg-slate-200"></div>
+      <div className="h-6 w-3/4 rounded-xl bg-slate-200"></div>
     </div>
-    <div className="h-4 w-[150px] rounded-md bg-slate-200"></div>
+    <div className="h-4 w-[150px] rounded-xl bg-slate-200"></div>
   </div>
 )
